26c4faba5ce8c3bc4da73e3f0094fb6fc8b22c5a
Stephanie Smith <stephanie.lauren.design@gmail.com>
2020-04-28 16:37:31 -0700

    using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,24 +2,25 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
-  it('should add two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    const calculator = new CalculatorService(logger);
+  let calculator: CalculatorService;
+  let loggerSpy: any;
 
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
+  it('should add two numbers', () => {
     const res = calculator.add(2, 2);
 
     expect(res).toBe(4);
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should add subtract numbers', () => {
-    const logger = new LoggerService();
-    const calculator = new CalculatorService(logger);
-    spyOn(logger, 'log');
-
     const res = calculator.subtract(6, 2);
 
     expect(res).toBe(4, 'Unexpected Subtraction Res');
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });

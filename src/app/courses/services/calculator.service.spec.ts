40f66a63c78955c433d2fb81988da65fbe52bc4b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2020-04-28 16:41:38 -0700

    configuring the test bed

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,4 +1,5 @@
 import { CalculatorService } from './calculator.service';
+import { TestBed } from '@angular/core/testing';
 import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
@@ -7,7 +8,15 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy }
+      ]
+    });
+
+    calculator = TestBed.get(CalculatorService);
   });
 
   it('should add two numbers', () => {

c6fdb6591f56ad612e743744605867072d53bb85
Stephanie Smith <stephanie.lauren.design@gmail.com>
2020-04-28 16:32:52 -0700

    working on the spy

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,13 +3,23 @@ import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    const calculator = new CalculatorService(logger);
+
     const res = calculator.add(2, 2);
+
     expect(res).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should add subtract numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+    const calculator = new CalculatorService(logger);
+    spyOn(logger, 'log');
+
     const res = calculator.subtract(6, 2);
-    expect(res).toBe(4);  });
+
+    expect(res).toBe(4, 'Unexpected Subtraction Res');
+    expect(logger.log).toHaveBeenCalledTimes(1);
+  });
 });
